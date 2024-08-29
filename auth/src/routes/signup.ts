ee2727dc215e8bd52f6f2b07827ddf7fc0e0fbc9
Siddharth Singh <siddharthsingh@Siddharths-MacBook-Pro.local>
2024-08-30 00:17:57 +0530

    Add express validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,29 @@
-import express from 'express'
+import express, { Response, Request} from 'express'
+import { body, validationResult } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/users/signup', (req, res) => {
-    res.send('SOmething')
+router.post('/api/users/signup',
+    [
+        body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+        body('password')
+        .trim()
+        .isLength({ min: 4, max: 20})
+        .withMessage('Password should be of minimus 4 and maximum 20 length')
+    ,
+    ],
+    (req: Request, res: Response) => {
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            return res.status(400).send(errors.array())
+        }
+        const { email, password } = req.body
+        console.log("creatinga  suer")
+        res.send({})
+        
 })
 
 export { router as signupRouter }
